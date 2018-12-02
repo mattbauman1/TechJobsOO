059282de012d88299a01619870290e6285002a02
Matt Bauman <mattbauman1@gmail.com>
2018-12-01 21:44:15 -0600

    Final project complete :)

--- src/TechJobs/ViewModels/BaseViewModel.cs
+++ src/TechJobs/ViewModels/BaseViewModel.cs
@@ -2,10 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TechJobs.Models;
 
 namespace TechJobs.ViewModels
 {
     public class BaseViewModel
     {
+        // The current column
+        public JobFieldType Column { get; set; } = JobFieldType.All;
+
+        // All columns, for display
+        public List<JobFieldType> Columns { get; set; }
+
+        // View title
+        public string Title { get; set; } = "";
+
+        public BaseViewModel()
+        {
+            Columns = new List<JobFieldType>();
+
+            foreach (JobFieldType enumVal in Enum.GetValues(typeof(JobFieldType)))
+            {
+                Columns.Add(enumVal);
+            }
+        }
     }
 }
